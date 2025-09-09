638aaa45b485f7cc2bc78ed076b3fdfc487068b5
Kristina DeSilva <97710440+kavdesilva@users.noreply.github.com>
2025-09-09 14:59:36 +0300

    details page created

--- frontend/src/pages/EditTea.jsx
+++ frontend/src/pages/EditTea.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const EditTea = (props) => {
   return (
     <div>
-      
+      <h1>Edit Teas</h1>
     </div>
   )
 };
--- frontend/src/pages/TeaDetails.jsx
+++ frontend/src/pages/TeaDetails.jsx
@@ -1,9 +1,32 @@
-import React from "react"
+import React, { use } from "react"
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom'
+import { showTea } from "../../utilities/teas-api";
+import teaPlaceHolder from '../assets/cute_tea.jpg'
 
 const TeaDetails = (props) => {
+  const { id } = useParams()
+  const [tea, setTea] = useState({})
+
+  useEffect(() => {
+    const loadTea = async () => {
+      try {
+        const teaData = await showTea(id)
+        setTea(teaData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    loadTea()
+  }, [id])
+
   return (
-    <div>
-      
+    <div className="tea-details">
+      <h1>Tea Details</h1>
+      <h1>{tea.name}</h1>
+      <img src={teaPlaceHolder} alt="cute-tea" className="tea-placeholder" />
+      <div className="tea-info"></div>
+      <div className="tea-actions"></div>
     </div>
   )
 };
